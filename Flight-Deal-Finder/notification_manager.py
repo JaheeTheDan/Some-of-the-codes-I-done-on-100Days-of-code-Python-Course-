04dd2becb6ebaafdff4f7926fd13f58b000a3b7e
JaheeTheDan <109768744+JaheeTheDan@users.noreply.github.com>
2022-11-02 13:49:24 -0500

    Update notification_manager.py

--- Flight-Deal-Finder/notification_manager.py
+++ Flight-Deal-Finder/notification_manager.py
@@ -4,8 +4,8 @@ class NotificationManager:
     '''Send a notification to user when a flight that is cheap'''
 
     def __init__(self,info):
-        self.__account_sid = 'AC934e582b848119c0ce6e6bf0a3a063d8'
-        self.__auth_token = '707839f8fb49a5b26371095f1f174e9e'
+        self.__account_sid = ''
+        self.__auth_token = ''
         self.client = Client(self.__account_sid, self.__auth_token)
         self.body = f"Low price alert! Only £{info['price']} to fly from {info['from_flight']} to {info['to_flight']}"\
             f"From {info['local_arrival_date']} to {info['local_departure_date']} "\
